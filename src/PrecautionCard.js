aab0c65753fd9bca7e14d5ba35a4cfff46cf5029
nrcool <nomanalam7214@gmail.com>
2021-11-22 18:39:27 +0100

    change image size

--- src/PrecautionCard.js
+++ src/PrecautionCard.js
@@ -4,7 +4,7 @@ export default function PrecautionCard({each,num}) {
 
     return (
         <div className={`precaution-card card${num}`}>
-            <img src={each.image} alt="" width="50" />
+            <img src={each.image} alt="" width="80" />
             <p>{each.text}</p>
         </div>
     )
